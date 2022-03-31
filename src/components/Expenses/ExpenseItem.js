9d6d18823ad9864582c2ca2809783d063ddbc90a
NateWalchenbach <natewalchenbach24@gmail.com>
2022-03-31 15:49:12 -0700

    Created a lot of stuff

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,21 @@
-import "./ExpenseItem.css";
-import ExpenseDate from "../Expenses/ExpenseDate";
-import Card from "../Ui/Card";
+import React from 'react';
 
-function ExpenseItem(props) {
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Card';
+import './ExpenseItem.css';
+
+const ExpenseItem = (props) => {
   return (
-    <Card className="expense-item">
-      <ExpenseDate date={props.date} />
-      <div className="expense-item__description">
-        <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.amount}</div>
-      </div>
-    </Card>
+    <li>
+      <Card className='expense-item'>
+        <ExpenseDate date={props.date} />
+        <div className='expense-item__description'>
+          <h2>{props.title}</h2>
+          <div className='expense-item__price'>${props.amount}</div>
+        </div>
+      </Card>
+    </li>
   );
-}
+};
 
-// Exporting default
 export default ExpenseItem;
